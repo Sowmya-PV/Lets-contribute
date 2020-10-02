9a03e9808e8b5177cd013b6d9dbbcd1c3ca3793a
Sowmya-PV <72245117+Sowmya-PV@users.noreply.github.com>
2020-10-02 16:56:37 +0530

    identifying the largest number among three

---
+++ pr1.py
@@ -0,0 +1,10 @@
+#largest number among the three
+num1=12
+num2=3
+num3=100
+if num1>num2 and num1>3:
+ print("num1 is greatest")
+elif num2>num1 and num2>num3:
+ print("num2 is greatest")
+else:
+ print("num3 is greatest")
